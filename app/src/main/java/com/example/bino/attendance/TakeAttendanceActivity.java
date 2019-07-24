5154bc1b38494d1ae89b221c140b633d6e19ddfe
amit gour <lokeshmali1997@gmail.com>
2019-07-24 21:21:21 +0530

    take attendance activity done

---
+++ app/src/main/java/com/example/bino/attendance/TakeAttendanceActivity.java
@@ -0,0 +1,13 @@
+package com.example.bino.attendance;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TakeAttendanceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_take_attendance);
+    }
+}

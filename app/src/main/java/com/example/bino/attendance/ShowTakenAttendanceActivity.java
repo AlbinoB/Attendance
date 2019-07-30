7ced19d5ea94007bf3fc1dba15cf6a36e6e6d20e
amit <gouramit20@gmail.com>
2019-07-30 17:15:03 +0530

    test

---
+++ app/src/main/java/com/example/bino/attendance/ShowTakenAttendanceActivity.java
@@ -0,0 +1,13 @@
+package com.example.bino.attendance;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ShowTakenAttendanceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_taken_attendance);
+    }
+}

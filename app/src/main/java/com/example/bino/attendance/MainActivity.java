e6abe39cd54b1d79c4cee9313453181dcf084522
albinoB <albinobraganza21@gmail.com>
2019-07-30 23:48:20 +0530

    Teacher Result individual student done

--- app/src/main/java/com/example/bino/attendance/MainActivity.java
+++ app/src/main/java/com/example/bino/attendance/MainActivity.java
@@ -11,7 +11,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Intent teacherHomeActivity1111 = new Intent(getApplicationContext(), TeacherSearchResult.class);
+        Intent teacherHomeActivity1111 = new Intent(getApplicationContext(), TeacherSearchResultIndividual.class);
         startActivity(teacherHomeActivity1111);
     }
 }

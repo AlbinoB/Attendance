30b2cf699f133e807b6a3d3ca8973a96d7ea625d
albinoB <albinobraganza21@gmail.com>
2019-07-31 20:23:09 +0530

    ficed calender in Teacher search by activity

--- app/src/main/java/com/example/bino/attendance/TeacherSearchByActivity.java
+++ app/src/main/java/com/example/bino/attendance/TeacherSearchByActivity.java
@@ -4,10 +4,18 @@ import android.app.DatePickerDialog;
 import android.icu.util.Calendar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.InputType;
+import android.util.Log;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.Spinner;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 public class TeacherSearchByActivity extends AppCompatActivity {
 
@@ -18,10 +26,79 @@ public class TeacherSearchByActivity extends AppCompatActivity {
     private static final String[] subjectNames = {"Select subject", "C++", "Java", "Android", "Advance Java"};
     //kk
 
+    EditText startDate;
+    EditText endDate;
+    DatePickerDialog datePickerDialogStartDate,datePickerDialogEndDate;
+    Calendar calendar;
+
+    private String getDateTime() {
+        DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd");
+        Date date = new Date();
+        return dateFormat.format(date);
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_teacher_search_by);
+
+        startDate=(EditText)findViewById(R.id.StartDate);
+        endDate=(EditText)findViewById(R.id.EndDate);
+        //startDate.setText(getDateTime());
+        startDate.setInputType(InputType.TYPE_NULL);//disable softkey board
+        endDate.setInputType(InputType.TYPE_NULL);
+
+        startDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String tempdate=getDateTime();
+                String[] dataarr=tempdate.split("/");
+                Log.i("date",getDateTime());
+                int day=Integer.parseInt(dataarr[2]);
+                int month=Integer.parseInt(dataarr[1])-1;
+                int year=Integer.parseInt(dataarr[0]);
+
+                datePickerDialogStartDate=new DatePickerDialog(TeacherSearchByActivity.this,
+                        new DatePickerDialog.OnDateSetListener() {
+                            @Override
+                            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                                startDate.setText((day+"/"+month+"/"+year));
+                            }
+                        },year,month,day);
+                datePickerDialogStartDate.show();
+            }
+            });
+
+        endDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String tempdate=getDateTime();
+                String[] dataarr=tempdate.split("/");
+                Log.i("date",getDateTime());
+                int day=Integer.parseInt(dataarr[2]);
+                int month=Integer.parseInt(dataarr[1])-1;
+                int year=Integer.parseInt(dataarr[0]);
+
+                datePickerDialogEndDate=new DatePickerDialog(TeacherSearchByActivity.this,
+                        new DatePickerDialog.OnDateSetListener() {
+                            @Override
+                            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                                endDate.setText((day+"/"+month+"/"+year));
+                            }
+                        },year,month,day);
+                datePickerDialogEndDate.show();
+            }
+        });
+
+
+
+
+
+
+
+
+
+
+
 
         SearchcourseSpiner = (Spinner)findViewById(R.id.SearchcourseSpinner);
         SearchyearSpiner = (Spinner)findViewById(R.id.SearchyearSpinner);

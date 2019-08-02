095fb4c438de1050ae67b6bb0159aa11ab4c9d51
albinoB <albinobraganza21@gmail.com>
2019-08-03 00:01:16 +0530

    AdminCourseparticular sem subs

--- app/src/main/java/com/example/bino/attendance/MainActivity.java
+++ app/src/main/java/com/example/bino/attendance/MainActivity.java
@@ -11,7 +11,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Intent loginActivityqq = new Intent(getApplicationContext(), AdminShowTeacherDetailsActivity.class);
+        Intent loginActivityqq = new Intent(getApplicationContext(), AdminCourseParticularSemSubjects.class);
         startActivity(loginActivityqq);
     }
 }

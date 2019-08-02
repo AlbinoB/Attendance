80cbca0c9a30aa7d70b8acce6da9883e817d2e84
amit <gouramit20@gmail.com>
2019-08-02 22:34:26 +0530

    admin teacher home page done

--- app/src/main/java/com/example/bino/attendance/AdminShowTeacherNameActivity.java
+++ app/src/main/java/com/example/bino/attendance/AdminShowTeacherNameActivity.java
@@ -2,49 +2,57 @@ package com.example.bino.attendance;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.Editable;
-import android.text.TextWatcher;
-import android.widget.ArrayAdapter;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
-
-import java.util.ArrayList;
+import android.widget.TextView;
 
 public class AdminShowTeacherNameActivity extends AppCompatActivity {
 
-  //  ArrayAdapter<String> teachernameadapter;
+    ListView teachernamelistview;
+
+     String[] teachernames={
+            "amit1", "amit2", "amit3", "amit4", "amit5", "amit6"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_show_teacher_name);
 
-     /*   ListView teachernamelistview= (ListView)findViewById(R.id.TEacherNameListView);
+        teachernamelistview= (ListView)findViewById(R.id.TEacherNameListView);
         EditText searchnametextview =(EditText) findViewById(R.id.SearchNameTextView);
-        ArrayList<String> teachername =new ArrayList<String>();
-        teachername.add("pankaj");
-        teachername.add("ankit");
-        teachername.add("jayyu");
-        teachername.add("radhe");
-        teachername.add("neeraj");
-         teachernameadapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,teachername);
-        teachernamelistview.setAdapter(teachernameadapter);
+        CustomAdapter customAdapter =new CustomAdapter();
+        teachernamelistview.setAdapter(customAdapter);
 
-        searchnametextview.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
 
-            }
+    }
+    class CustomAdapter extends BaseAdapter{
 
-            @Override
-            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-                (AdminShowTeacherNameActivity.this).teachernameadapter.getFilter().filter(charSequence);
-            }
+        @Override
+        public int getCount() {
+            return teachernames.length;
+        }
 
-            @Override
-            public void afterTextChanged(Editable editable) {
+        @Override
+        public Object getItem(int i) {
+            return null;
+        }
 
-            }
-        });*/
+        @Override
+        public long getItemId(int i) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int i, View view, ViewGroup viewGroup) {
+            view = getLayoutInflater().inflate(R.layout.custonadminshowteachername,null);
+            TextView teachernametextview =(TextView)(view).findViewById(R.id.TeacherNameAddTextView);
+
+            teachernametextview.setText(teachernames[i]);
+
+            return view;
+        }
     }
 }

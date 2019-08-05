dd2af4b4c2a73286b6dfa30defa960ac7a25edd1
albinoB <albinobraganza21@gmail.com>
2019-08-05 14:12:33 +0530

    dynamic loading of subject from db to studentview all subject

--- app/src/main/java/com/example/bino/attendance/LoginActivity.java
+++ app/src/main/java/com/example/bino/attendance/LoginActivity.java
@@ -1,6 +1,7 @@
 package com.example.bino.attendance;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.AsyncTask;
 import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
@@ -25,147 +26,170 @@ import java.text.SimpleDateFormat;
 
 public class LoginActivity extends AppCompatActivity {
 
-    private static final String[] users ={"Select User","Teacher","Student","Admin"};
-    Spinner typeOfUser;
-    EditText usernametext,passwordtext;
 
-    String userName="",password="",user="";
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);//full screen login activity
-        setContentView(R.layout.activity_login);
+        private static final String[] users ={"Select User","Teacher","Student","Admin"};
+        Spinner typeOfUser;
+        EditText usernametext,passwordtext;
+
+        String userName="",password="",user="";
+
+        SharedPreferences sharedPreferences;
 
 
-        typeOfUser=(Spinner)findViewById(R.id.typeOfUser);
-        usernametext=(EditText) findViewById(R.id.userNameEditText);
-        passwordtext=(EditText) findViewById(R.id.passwordEditText);
 
-        ArrayAdapter<String> userAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_spinner_dropdown_item,users);
+        public class ConnectToDB extends AsyncTask<String,Void,Boolean>{
+            @Override
+            protected Boolean doInBackground(String... sqlarr) {
+                StrictMode.ThreadPolicy policy=new StrictMode.ThreadPolicy.Builder().permitAll().build();
+                StrictMode.setThreadPolicy(policy);
+                Connection connection=null;
+                String url=null;
+                try {
+                    Class.forName("net.sourceforge.jtds.jdbc.Driver");
+                    url = "jdbc:jtds:sqlserver://androidattendancedbserver.database.windows.net:1433;DatabaseName=AndroidAttendanceDB;user=AlbinoAmit@androidattendancedbserver;password=AAnoit$321;encrypt=true;trustServerCertificate=false;hostNameInCertificate=*.database.windows.net;loginTimeout=30;";
+                    connection= DriverManager.getConnection(url);
+                    Statement stmt = connection.createStatement();
+                    ResultSet rs=null;
+                    rs = stmt.executeQuery(sqlarr[0]);
+                    //Log.i("data:::::::::::::",Integer.toString(rs.getInt(1)));
 
-        typeOfUser.setAdapter(userAdapter);
+                    Log.i("data:::::::::::::",sqlarr[0]);
 
-    }
-            public void Login(View view) {
+                    if(rs.next()){
+                        return true;
+                    }else
+                    {
+                        return false;
+                    }
+                }
+                catch (Exception e){
+                    e.printStackTrace();
+                    return  false;
+                }
+            }
+        }
 
-                userName = usernametext.getText().toString();
-                password = passwordtext.getText().toString();
-                Log.i("llllllllllllllllllll",userName +" "+password);
-                user = typeOfUser.getSelectedItem().toString();
-                if (checkEmptyFields()) {
 
-                    switch (user) {
-                        case "Select User": {
-                            Toast.makeText(this, "Please select Type of User!!!", Toast.LENGTH_LONG).show();
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+            getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);//full screen login activity
+            setContentView(R.layout.activity_login);
+
+
+            typeOfUser=(Spinner)findViewById(R.id.typeOfUser);
+            usernametext=(EditText) findViewById(R.id.userNameEditText);
+            passwordtext=(EditText) findViewById(R.id.passwordEditText);
+
+            ArrayAdapter<String> userAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_spinner_dropdown_item,users);
+
+            typeOfUser.setAdapter(userAdapter);
+
+        }
+        public void Login(View view) throws Exception{
+            ConnectToDB connectToDB=new ConnectToDB();//obj of async class
+
+            userName = usernametext.getText().toString();
+            password = passwordtext.getText().toString();
+            Log.i("llllllllllllllllllll",userName +" "+password);
+            user = typeOfUser.getSelectedItem().toString();
+            if (checkEmptyFields()) {
+
+                switch (user) {
+                    case "Select User": {
+                        Toast.makeText(this, "Please select Type of User!!!", Toast.LENGTH_LONG).show();
+                    }
+                    break;
+                    case "Teacher": {
+                        String[] sql = {"SELECT teacherId FROM Teacher where teacherName= '" + userName + "' and teacherPassword='" + password+"' "};
+
+                        if(connectToDB.execute(sql).get()&&user.equals("Teacher")){
+                            Toast.makeText(this, "Welcome "+userName, Toast.LENGTH_LONG).show();
+                            Intent teacherHomeActivity = new Intent(getApplicationContext(), TeacherHomeActivity.class);
+
+                            sharedPreferences=this.getApplicationContext().getSharedPreferences("om.example.bino.attendance",MODE_PRIVATE);
+                            sharedPreferences.edit().putString("currentUserName",userName).apply();
+
+                            startActivity(teacherHomeActivity);
+                        }else
+                        {
+                            Toast.makeText(this, "Wrong Credentials!!!", Toast.LENGTH_LONG).show();
                         }
-                        break;
-                        case "Teacher": {
-                            String sql = "SELECT teacherId FROM Teacher where teacherName= '" + userName + "' and teacherPassword='" + password+"' ";
-                            if(connectionClass(sql)&&user.equals("Teacher")){
-                                Toast.makeText(this, "Welcome "+userName, Toast.LENGTH_LONG).show();
-                                Intent teacherHomeActivity = new Intent(getApplicationContext(), TeacherHomeActivity.class);
-                                startActivity(teacherHomeActivity);
-                            }else
-                            {
-                                Toast.makeText(this, "Wrong User!!! ", Toast.LENGTH_LONG).show();
-                            }
-                        }
-                        break;
-                        case "Student": {
-                            String sql = "SELECT studentErpNo FROM Student where studentName= '" + userName + "' and studentPassword= '" + password+"' ";
-                            if(connectionClass(sql)&&user.equals("Student")){
-                                Toast.makeText(this, "Welcome "+userName, Toast.LENGTH_LONG).show();
-                                Intent studentViewAllAttendance = new Intent(getApplicationContext(), StudentViewAllAttendance.class);
-                                startActivity(studentViewAllAttendance);
-                            }else
-                            {
-                                Toast.makeText(this, "Wrong User!!! ", Toast.LENGTH_LONG).show();
-                            }
+                    }
+                    break;
+                    case "Student": {
+                        String sql = "SELECT studentErpNo FROM Student where studentName= '" + userName + "' and studentPassword= '" + password+"' ";
+                        if(connectToDB.execute(sql).get()&&user.equals("Student")){
+                            Toast.makeText(this, "Welcome "+userName, Toast.LENGTH_LONG).show();
+                            Intent studentViewAllAttendance = new Intent(getApplicationContext(), StudentViewAllAttendance.class);
 
-                        }
-                        break;
-                        case "Admin": {
-                            if (userName.equals("admin") && password.equals("admin") && user.equals("Admin")) {
-                                Toast.makeText(this, "Welcome "+userName, Toast.LENGTH_LONG).show();
-                                Intent adminhomeactivity = new Intent(getApplicationContext(), AdminHomeActivity.class);
-                                startActivity(adminhomeactivity);
-                            }else
-                            {
-                                Toast.makeText(this, "Wrong Credentials!!! ", Toast.LENGTH_LONG).show();
-                            }
-                            break;
+                            sharedPreferences=this.getApplicationContext().getSharedPreferences("om.example.bino.attendance",MODE_PRIVATE);
+                            sharedPreferences.edit().putString("currentUserName",userName).apply();
 
+                            startActivity(studentViewAllAttendance);
+                        }else
+                        {
+                            Toast.makeText(this, "Wrong Credentials!!!", Toast.LENGTH_LONG).show();
                         }
 
                     }
+                    break;
+                    case "Admin": {
+                        if (userName.equals("admin") && password.equals("admin") && user.equals("Admin")) {
+                            Toast.makeText(this, "Welcome "+userName, Toast.LENGTH_LONG).show();
+                            Intent adminhomeactivity = new Intent(getApplicationContext(), AdminHomeActivity.class);
+                            sharedPreferences=this.getApplicationContext().getSharedPreferences("om.example.bino.attendance",MODE_PRIVATE);
+                            sharedPreferences.edit().putString("currentUserName",userName).apply();
 
+                            startActivity(adminhomeactivity);
+                        }else
+                        {
+                            Toast.makeText(this, "Wrong Credentials!!! ", Toast.LENGTH_LONG).show();
+                        }
+                        break;
+
+                    }
+
+                }
+
+            }
+        }
+
+        public boolean checkEmptyFields(){
+            String error="";
+            if(userName.equals("")&&userName.equals("")){
+                error="Please enter User Name and Password!!!";
+                passwordtext.setText("");
+            }else
+            {
+                if(userName.equals("")){
+                    error="Please enter User Name!!!";
+                }
+                else {
+                    if(password.equals("")){
+                        error="Please enter Password!!!";
+                    }
                 }
             }
 
-            public boolean checkEmptyFields(){
-                String error="";
-                if(userName.equals("")&&userName.equals("")){
-                    error="Please enter User Name and Password!!!";
-                    passwordtext.setText("");
-                }else
-                {
-                    if(userName.equals("")){
-                        error="Please enter User Name!!!";
-                    }
-                    else {
-                        if(password.equals("")){
-                            error="Please enter Password!!!";
-                        }
-                    }
-                }
-
-                if(error!="")
-                {
-                    Toast.makeText(this, error, Toast.LENGTH_LONG).show();
-                    return false;
-                }
-                else
-                {
-                    return true;
-                }
-
+            if(error!="")
+            {
+                Toast.makeText(this, error, Toast.LENGTH_LONG).show();
+                return false;
             }
-
-
-    public boolean connectionClass(String sql){
-        StrictMode.ThreadPolicy policy=new StrictMode.ThreadPolicy.Builder().permitAll().build();
-        StrictMode.setThreadPolicy(policy);
-        Connection connection=null;
-        String url=null;
-        try {
-            Class.forName("net.sourceforge.jtds.jdbc.Driver");
-            url = "jdbc:jtds:sqlserver://androidattendancedbserver.database.windows.net:1433;DatabaseName=AndroidAttendanceDB;user=AlbinoAmit@androidattendancedbserver;password=AAnoit$321;encrypt=true;trustServerCertificate=false;hostNameInCertificate=*.database.windows.net;loginTimeout=30;";
-            connection= DriverManager.getConnection(url);
-            Statement stmt = connection.createStatement();
-            ResultSet rs=null;
-            rs = stmt.executeQuery(sql);
-            //Log.i("data:::::::::::::",Integer.toString(rs.getInt(1)));
-
-            Log.i("data:::::::::::::",sql);
-
-            if(rs.next()){
+            else
+            {
                 return true;
-                }else
-                 {
-                     Toast.makeText(this, "Wrong Credentials!!!", Toast.LENGTH_LONG).show();
-                     return false;
-                 }
+            }
+
         }
-        catch (Exception e){
-            e.printStackTrace();
-            return  false;
-        }
+
+
+
     }
 
-
-
-}
 
 
 
--- app/src/main/java/com/example/bino/attendance/StudentViewAllAttendance.java
+++ app/src/main/java/com/example/bino/attendance/StudentViewAllAttendance.java
@@ -1,8 +1,12 @@
 package com.example.bino.attendance;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.AsyncTask;
+import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
@@ -11,22 +15,90 @@ import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.Arrays;
+import java.util.concurrent.ExecutionException;
+
 public class StudentViewAllAttendance extends AppCompatActivity {
-    static String[][] studentsarr =
+          String[][] studentsarr =
             {
-                    {"3001", "Advance Java", "90.2"},
-                    {"3001", "Advance Java", "90.2"},
-                    {"3001", "Advance Java", "90.2"},
-                    {"3001", "Advance Java", "90.2"},
-                    {"3001", "Advance Java", "90.2"},
-                    {"3001", "Advance Java", "90.2"},
-                    {"3001", "Advance Java", "90.2"},
-                    {"3001", "Advance Java", "90.2"},
                     {"3001", "Advance Java", "90.2"},
                     {"3001", "Advance Java", "90.2"},
                     {"3001", "Advance Java", "90.2"}
             };
+
+
     ListView studentlistView;
+
+    SharedPreferences sharedPreferences;
+
+    TextView currentUserTextView,studentRollNoTextView,studentCourseNameTextView;
+
+    CustomAdapter customAdapter;
+
+
+    public class ConnectToDB extends AsyncTask<String,Void,Boolean>{
+        @Override
+        protected Boolean doInBackground(String... sqlarr) {
+            StrictMode.ThreadPolicy policy=new StrictMode.ThreadPolicy.Builder().permitAll().build();
+            StrictMode.setThreadPolicy(policy);
+            Connection connection=null;
+            String url=null;
+            try {
+                Class.forName("net.sourceforge.jtds.jdbc.Driver");
+                url = "jdbc:jtds:sqlserver://androidattendancedbserver.database.windows.net:1433;DatabaseName=AndroidAttendanceDB;user=AlbinoAmit@androidattendancedbserver;password=AAnoit$321;encrypt=true;trustServerCertificate=false;hostNameInCertificate=*.database.windows.net;loginTimeout=30;";
+                connection= DriverManager.getConnection(url);
+                Statement stmt = connection.createStatement();
+                ResultSet rs=null;
+                String sql=sqlarr[0];
+
+                Log.i("data:::::::::::::",sql);
+
+                rs = stmt.executeQuery(sql);
+
+
+
+                if(rs.next())
+                studentCourseNameTextView.setText(rs.getString("courseName"));
+                else{
+                    Log.i("nothing","nothing");
+                }
+
+
+
+                sql="select subjectId,subjectName from Subject,Course where fkcourseIdSubject =courseId and  courseName='"+studentCourseNameTextView.getText().toString()+"'";
+
+                Log.i("data:::::::cccc::::::",sql);
+
+                rs=null;
+                rs = stmt.executeQuery(sql);
+                int indexOfstudentarr=0;
+                while(rs.next()){
+                    Log.i("values from db:",Integer.toString(rs.getInt("subjectId"))+rs.getString("subjectName"));
+
+
+                   // studentsarr= Arrays.copyOf(studentsarr, studentsarr.length + 1);
+                    studentsarr[indexOfstudentarr][0]=Integer.toString(rs.getInt("subjectId"));
+                    studentsarr[indexOfstudentarr][1]=rs.getString("subjectName");
+                    studentsarr[indexOfstudentarr][2]="";
+                    indexOfstudentarr++;
+
+                }
+                return true;
+            }
+            catch (Exception e){
+                e.printStackTrace();
+                return  false;
+            }
+        }
+    }
+
+
+
+
 
 
 
@@ -34,49 +106,85 @@ public class StudentViewAllAttendance extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_student_view_all_attendance);
+
+
+
+        sharedPreferences=this.getApplicationContext().getSharedPreferences("om.example.bino.attendance",MODE_PRIVATE);
+
+
+
+        currentUserTextView=(TextView)findViewById(R.id.currentUser);
+        studentRollNoTextView=(TextView)findViewById(R.id.studentRollNo);
+        studentCourseNameTextView=(TextView)findViewById(R.id.studentCourseName);
+
+        currentUserTextView.setText((String)sharedPreferences.getString("currentUserName","no  name"));
+
+
+        ConnectToDB connectToDB=new ConnectToDB();//obj of async class
+        String[] sql={
+                "select courseName from Course where courseId=(select fkcourseIdStudent from Student where studentName='"+currentUserTextView.getText().toString()+"')",
+        };
+        try {
+            if(connectToDB.execute(sql).get()){
+                {
+                    Log.i("updated:mmmmm","doneee");
+
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+
+
         studentlistView=(ListView)findViewById(R.id.listView);
-        CustomAdapter customAdapter=new CustomAdapter();
+        customAdapter=new CustomAdapter();
         studentlistView.setAdapter(customAdapter);
+
 
         studentlistView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
                 Intent studentviewindividualattendance = new Intent(getApplicationContext(), StudentViewIndividualAttendance.class);
+                studentviewindividualattendance.putExtra("item index selected",i);
                 startActivity(studentviewindividualattendance);
             }
         });
 
+        Log.i("contents of studentarr",studentsarr[0][0]+studentsarr[0][1]+studentsarr[0][2]);
+
     }
 
-        public class CustomAdapter extends BaseAdapter {
+    public class CustomAdapter extends BaseAdapter {
 
-            @Override
-            public int getCount() {
-                return studentsarr.length;
-            }
-
-            @Override
-            public Object getItem(int i) {
-                return null;
-            }
-
-            @Override
-            public long getItemId(int i) {
-                return 0;
-            }
-
-            @Override
-            public View getView(int i, View view, ViewGroup viewGroup) {
-                view = getLayoutInflater().inflate(R.layout.customlayoutstudentallattendance, null);
-                TextView textViewScode = (TextView) view.findViewById(R.id.sCode);
-                TextView textViewSname = (TextView) view.findViewById(R.id.sName);
-                TextView textViewSpercentage= (TextView) view.findViewById(R.id.sPercentage);
-                textViewScode.setText(studentsarr[i][0]);
-                textViewSname.setText(studentsarr[i][1]);
-                textViewSpercentage.setText(studentsarr[i][2]);
-
-                return view;
-            }
+        @Override
+        public int getCount() {
+            return studentsarr.length;
         }
+
+        @Override
+        public Object getItem(int i) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int i) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int i, View view, ViewGroup viewGroup) {
+            view = getLayoutInflater().inflate(R.layout.customlayoutstudentallattendance, null);
+            TextView textViewScode = (TextView) view.findViewById(R.id.sCode);
+            TextView textViewSname = (TextView) view.findViewById(R.id.sName);
+            TextView textViewSpercentage= (TextView) view.findViewById(R.id.sPercentage);
+            textViewScode.setText(studentsarr[i][0]);
+            textViewSname.setText(studentsarr[i][1]);
+            textViewSpercentage.setText(studentsarr[i][2]);
+
+            return view;
+        }
+    }
 
 }

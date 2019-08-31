4a00c599c95dd5cb6825c0a41eaa2c0f23e66218
AlbinoB <albinobraganza21@gmail.com>
2019-08-31 21:15:24 +0530

    login dialogue progress on login activity

--- app/src/main/java/com/example/bino/attendance/LoginActivity.java
+++ app/src/main/java/com/example/bino/attendance/LoginActivity.java
@@ -4,6 +4,8 @@ import android.app.ProgressDialog;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.AsyncTask;
+import android.os.Handler;
+import android.os.Looper;
 import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -24,6 +26,7 @@ import java.sql.ResultSet;
 import java.sql.Statement;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.concurrent.ExecutionException;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -36,6 +39,7 @@ public class LoginActivity extends AppCompatActivity {
 
         SharedPreferences sharedPreferences;
         ProgressDialog progressdialog;
+        Handler handler =new Handler();
 
 
 
@@ -101,6 +105,7 @@ public class LoginActivity extends AppCompatActivity {
             setContentView(R.layout.activity_login);
 
 
+
             typeOfUser=(Spinner)findViewById(R.id.typeOfUser);
             usernametext=(EditText) findViewById(R.id.userNameEditText);
             passwordtext=(EditText) findViewById(R.id.passwordEditText);
@@ -111,94 +116,153 @@ public class LoginActivity extends AppCompatActivity {
             typeOfUser.setAdapter(userAdapter);
 
         }
+        @Override
+        protected void onPause(){
+            super.onPause();
+            progressdialog.dismiss();
+        }
         public void Login(View view) throws Exception{
 
-
-            progressdialog.setMessage("Please Wait....");
-            progressdialog.show();
-
-            ConnectToDB connectToDB=new ConnectToDB();//obj of async class
 
             userName = usernametext.getText().toString();
             password = passwordtext.getText().toString();
             Log.i("llllllllllllllllllll",userName +" "+password);
             user = typeOfUser.getSelectedItem().toString();
-            if (checkEmptyFields()) {
 
-                switch (user) {
-                    case "Select User": {
-                        Toast.makeText(this, "Please select Type of User!!!", Toast.LENGTH_LONG).show();
-                        if (progressdialog.isShowing()) {
-                            progressdialog.dismiss();
+
+            progressdialog.setMessage("Please Wait....");
+            progressdialog.show();
+
+            Thread validationThread=new Thread(new Runnable() {
+
+                ConnectToDB connectToDB=new ConnectToDB();//obj of async class
+                @Override
+                public void run() {
+                    Looper.prepare();
+                    if (checkEmptyFields()) {
+                        try{
+                        switch (user) {
+
+
+                            case "Select User": {
+                                handler.post(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        Toast.makeText(LoginActivity.this, "Please select Type of User!!!", Toast.LENGTH_LONG).show();
+
+                                    }
+                                });
+
+                            }
+                            break;
+                            case "Teacher": {
+                                String[] sql = {"SELECT teacherName as currentUserName FROM Teacher where teacherId= '" + userName + "' and teacherPassword='" + password + "' "};
+
+
+                                if (connectToDB.execute(sql).get() && user.equals("Teacher")) {
+                                    handler.post(new Runnable() {
+                                        @Override
+                                        public void run() {
+                                            Toast.makeText(LoginActivity.this, "Welcome " + (String) sharedPreferences.getString("currentUserName", "no  name"), Toast.LENGTH_LONG).show();
+                                        }
+                                    });
+
+                                    sharedPreferences.edit().putInt("currentUserId", Integer.parseInt(userName)).apply();
+                                    Intent teacherHomeActivity = new Intent(getApplicationContext(), TeacherHomeActivity.class);
+
+                                    startActivity(teacherHomeActivity);
+                                } else {
+                                    handler.post(new Runnable() {
+                                        @Override
+                                        public void run() {
+                                            progressdialog.dismiss();
+                                            Toast.makeText(LoginActivity.this, "Wrong Credentials!!!", Toast.LENGTH_LONG).show();
+
+                                        }
+                                    });
+
+
+                                }
+
+                            }
+                            break;
+                            case "Student": {
+                                String sql = "SELECT studentName as currentUserName FROM Student where studentErpNo= '" + userName + "' and studentPassword= '" + password+"' ";
+                                if(connectToDB.execute(sql).get()&&user.equals("Student")){
+                                    handler.post(new Runnable() {
+                                        @Override
+                                        public void run() {
+                                            Toast.makeText(LoginActivity.this, "Welcome "+(String)sharedPreferences.getString("currentUserName","no  name"), Toast.LENGTH_LONG).show();
+
+                                        }
+                                    });
+
+                                    Intent studentViewAllAttendance = new Intent(getApplicationContext(), StudentViewAllAttendance.class);
+
+                                    sharedPreferences.edit().putInt("currentUserErpNo",Integer.parseInt(userName)).apply();
+                                    startActivity(studentViewAllAttendance);
+                                }else
+                                {
+                                    handler.post(new Runnable() {
+                                        @Override
+                                        public void run() {
+                                            progressdialog.dismiss();
+                                            Toast.makeText(LoginActivity.this, "Wrong Credentials!!!", Toast.LENGTH_LONG).show();
+
+                                        }
+                                    });
+
+
+                                }
+
+                            }
+                            break;
+                            case "Admin": {
+                                if (userName.equals("admin") && password.equals("admin") && user.equals("Admin")) {
+                                    handler.post(new Runnable() {
+                                        @Override
+                                        public void run() {
+                                            Toast.makeText(LoginActivity.this, "Welcome "+userName, Toast.LENGTH_LONG).show();
+
+                                        }
+                                    });
+
+                                    Intent adminhomeactivity = new Intent(getApplicationContext(), AdminHomeActivity.class);
+                                    sharedPreferences.edit().putString("currentUserName",userName).apply();
+
+                                    startActivity(adminhomeactivity);
+                                }else
+                                {
+                                    handler.post(new Runnable() {
+                                        @Override
+                                        public void run() {
+                                            progressdialog.dismiss();
+                                            Toast.makeText(LoginActivity.this, "Wrong Credentials!!! ", Toast.LENGTH_LONG).show();
+
+                                        }
+                                    });
+
+
+                                }
+                                break;
+
+                            }
+
                         }
-                    }
-                    break;
-                    case "Teacher": {
-                        String[] sql = {"SELECT teacherName as currentUserName FROM Teacher where teacherId= '" + userName + "' and teacherPassword='" + password+"' "};
 
-                        if(connectToDB.execute(sql).get()&&user.equals("Teacher")){
-                            Toast.makeText(this, "Welcome "+(String)sharedPreferences.getString("currentUserName","no  name"), Toast.LENGTH_LONG).show();
-                            sharedPreferences.edit().putInt("currentUserId",Integer.parseInt(userName)).apply();
-                            Intent teacherHomeActivity = new Intent(getApplicationContext(), TeacherHomeActivity.class);
-
-                            startActivity(teacherHomeActivity);
-                            if (progressdialog.isShowing()) {
-                                progressdialog.dismiss();
-                            }
-                        }else
-                        {
-                            if (progressdialog.isShowing()) {
-                                progressdialog.dismiss();
-                            }
-                            Toast.makeText(this, "Wrong Credentials!!!", Toast.LENGTH_LONG).show();
+                    } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        } catch (ExecutionException e) {
+                            e.printStackTrace();
                         }
-                    }
-                    break;
-                    case "Student": {
-                        String sql = "SELECT studentName as currentUserName FROM Student where studentErpNo= '" + userName + "' and studentPassword= '" + password+"' ";
-                        if(connectToDB.execute(sql).get()&&user.equals("Student")){
-                            Toast.makeText(this, "Welcome "+(String)sharedPreferences.getString("currentUserName","no  name"), Toast.LENGTH_LONG).show();
-                            Intent studentViewAllAttendance = new Intent(getApplicationContext(), StudentViewAllAttendance.class);
-
-                            sharedPreferences.edit().putInt("currentUserErpNo",Integer.parseInt(userName)).apply();
-                            if (progressdialog.isShowing()) {
-                                progressdialog.dismiss();
-                            }
-                            startActivity(studentViewAllAttendance);
-                        }else
-                        {
-                            if (progressdialog.isShowing()) {
-                                progressdialog.dismiss();
-                            }
-                            Toast.makeText(this, "Wrong Credentials!!!", Toast.LENGTH_LONG).show();
-                        }
-
-                    }
-                    break;
-                    case "Admin": {
-                        if (userName.equals("admin") && password.equals("admin") && user.equals("Admin")) {
-                            Toast.makeText(this, "Welcome "+userName, Toast.LENGTH_LONG).show();
-                            Intent adminhomeactivity = new Intent(getApplicationContext(), AdminHomeActivity.class);
-                            sharedPreferences=this.getApplicationContext().getSharedPreferences("om.example.bino.attendance",MODE_PRIVATE);
-                            sharedPreferences.edit().putString("currentUserName",userName).apply();
-                            if (progressdialog.isShowing()) {
-                                progressdialog.dismiss();
-                            }
-                            startActivity(adminhomeactivity);
-                        }else
-                        {
-                            if (progressdialog.isShowing()) {
-                                progressdialog.dismiss();
-                            }
-                            Toast.makeText(this, "Wrong Credentials!!! ", Toast.LENGTH_LONG).show();
-                        }
-                        break;
-
                     }
 
                 }
+            });
 
-            }
+
+
+            validationThread.start();
 
         }
 

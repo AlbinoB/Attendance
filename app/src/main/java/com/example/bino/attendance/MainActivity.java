3a38c14fd13663c2d852e21e49342709564552d7
albinoB <albinobraganza21@gmail.com>
2019-09-21 15:17:44 +0530

    flash screen done

--- app/src/main/java/com/example/bino/attendance/MainActivity.java
+++ app/src/main/java/com/example/bino/attendance/MainActivity.java
@@ -6,21 +6,37 @@ import java.text.SimpleDateFormat;
 import java.util.*;
 
 import android.content.Intent;
+import android.os.Handler;
 import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
 
 public class MainActivity extends AppCompatActivity {
 
+    /** Duration of wait **/
+    private final int SPLASH_DISPLAY_LENGTH = 3000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Intent loginActivity = new Intent(getApplicationContext(), LoginActivity.class);
-        finish();
-        startActivity(loginActivity);
+           /* New Handler to start the Menu-Activity
+         * and close this Splash-Screen after some seconds.*/
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run() {
+                /* Create an Intent that will start the Menu-Activity. */
+                Intent mainIntent = new Intent(MainActivity.this,LoginActivity.class);
+                MainActivity.this.startActivity(mainIntent);
+                MainActivity.this.finish();
+            }
+        }, SPLASH_DISPLAY_LENGTH);
+
+       // Intent loginActivity = new Intent(getApplicationContext(), LoginActivity.class);
+       // finish();
+      //  startActivity(loginActivity);
         //connectionClass();
     }
 /*
